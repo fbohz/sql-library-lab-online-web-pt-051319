0b2d936db13572bd18f09760e96f8d5ef10c2623
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-07-16 17:51:19 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,5 @@
 INSERT INTO series (title) VALUES ("hi"), ("elo");
 INSERT INTO subgenres (id, name) VALUES (1, "ggfs"), (2, "gfdha");
 INSERT INTO authors (id, name) VALUES (1, "me"), (2, "you");
+INSERT INTO books (id, title, year, series_id) VALUES (1, "54352", 5434, 1), (2, "A gfsdgs of gfsd", 256, 5), (3, "5432", 5433, 4), (4, "fdas", 2102, 2), (5, "2 Book", 2015, 2), (6, "Book", 2041, 2);
 
