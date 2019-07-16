1f240ca0c4c4e7b51f80cf076ab8a4bffb1f04d2
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-07-16 17:49:56 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,4 @@
 INSERT INTO series (title) VALUES ("hi"), ("elo");
 INSERT INTO subgenres (id, name) VALUES (1, "ggfs"), (2, "gfdha");
 INSERT INTO authors (id, name) VALUES (1, "me"), (2, "you");
+
